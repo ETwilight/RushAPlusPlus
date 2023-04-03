69b49633f8388b87d571472fd5b8c64cdf2ceedb
WilliamOuJiatong <109175850+WilliamOuJiatong@users.noreply.github.com>
2023-04-02 19:36:18 -0500

    Create Page.hpp

---
+++ Page.hpp
@@ -0,0 +1,16 @@
+class Program::Page
+{
+    // Default Constructor for a Page class;
+    // Current Usage: The default Page() is used for a new user;
+    Page();
+
+    // The Constructor for a Page class that take in a user_id;
+    // Current Usage: Generate a Page() for a user with user_id;
+    Page(User user_id);
+
+    // A list of videos that is pushed to the user in this Page();
+    std::vector<Video> video_list;
+        
+    // 
+    Video currentVideo;
+}
