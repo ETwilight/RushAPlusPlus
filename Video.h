49e52ace25f65f0eb9281ad16837c1fd15ee9c53
WilliamOuJiatong <109175850+WilliamOuJiatong@users.noreply.github.com>
2023-04-02 19:49:27 -0500

    Update Video.h

--- Video.h
+++ Video.h
@@ -7,5 +7,6 @@ class Program::Video
     // The intrinsic tag of the Video;
     // Used to make automatic click on the highest matching video;
     private;
+    std::string name_;
     std::vector<std::pair<std::string, double>> tag_;
 };
